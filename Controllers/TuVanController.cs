e28c1fa9e014673fa8832fd53e3720a4c5665494
YenTrinh13 <hoangthiyentrinh2001@gmail.com>
2022-08-25 22:01:45 +0700

    vatlieu

---
+++ Controllers/TuVanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuaHangVatLieuXayDung.Controllers
+{
+    public class TuVanController : Controller
+    {
+        // GET: TuVan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
